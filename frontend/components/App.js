f466b57ffbb22ee5fa8fa26238a2d8478bfdc166
Usna Rafiqzadah <usna.rafiqzadah@gmail.com>
2023-07-11 16:52:21 -0400

    saving project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './TodoList';
+import Form from './Form';
+
+
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+
+      ]
+    }
+  }
+
+
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
-        Todo App
+        <h1> Todos: </h1>
+        
+        <TodoList todos={todos}/>
+        <Form />
+        
+        <button>Hide Completed</button>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 export default class Form extends React.Component {
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form>
+          <input />
+          <button>Submit</button>
+      </form>
     )
   }
 }
